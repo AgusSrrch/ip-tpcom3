cd3ba0270e7c54517ee42372e79ad5cf03df5b8b
ItsStreamStyles <lucasgreco43@gmail.com>
2024-06-24 13:24:28 -0300

    Update services_nasa_image_gallery.py

--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -5,12 +5,12 @@ from ..dao import repositories
 from nasa_image_gallery.layers.generic.mapper import fromRequestIntoNASACard
 from django.contrib.auth import get_user
 
-def GetAllImages(input=None):
+def getAllImagesfromTransport(input=None):
     # obtiene un listado de imágenes desde transport.py y lo guarda en un json_collection.
     # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
     images = []
-    json_collection = getALLImages(input)
-    for obj in json_collection:
+    json_collection = getAllImages(input)
+    for obj in json_collection: 
         nasa_card = fromRequestIntoNASACard(obj)
         images.append(nasa_card)
     # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
@@ -18,8 +18,14 @@ def GetAllImages(input=None):
     return images
 
 
+
 def getImagesBySearchInputLike(input):
-    return getAllImages(input)
+    json_collection = getAllImages(input)
+    images = []
+    for obj in json_collection:
+        nasa_card = fromRequestIntoNASACard(obj)
+        images.append(nasa_card)
+    return images
 
 
 # añadir favoritos (usado desde el template 'home.html')
@@ -31,17 +37,16 @@ def saveFavourite(request):
 
 
 # usados en el template 'favourites.html'
-def getAllFavouritesByUser(request):
+def get_Favourites_User(request):
     if not request.user.is_authenticated:
         return []
     else:
         user = get_user(request)
-
-        favourite_list = [] # buscamos desde el repositorio TODOS los favoritos del usuario (variable 'user').
+        favourite_list = GetAllFavouritesByUser(user)# buscamos desde el repositorio TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
 
         for favourite in favourite_list:
-            nasa_card = '' # transformamos cada favorito en una NASACard, y lo almacenamos en nasa_card.
+            nasa_card = fromRepositoryIntoNASACard(favourite) # transformamos cada favorito en una NASACard, y lo almacenamos en nasa_card.
             mapped_favourites.append(nasa_card)
 
         return mapped_favourites
